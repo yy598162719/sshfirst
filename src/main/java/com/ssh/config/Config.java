241f1f8b7429ab4c9f469a9ad358616c2045c7cf
lwj <598162719@qq.com>
2018-06-29 22:04:00 +0800

    111

--- src/main/java/com/ssh/config/Config.java
+++ src/main/java/com/ssh/config/Config.java
@@ -2,8 +2,9 @@ package com.ssh.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-
+//测试???
 @Configuration
 @ComponentScan(basePackages = "com.ssh")
 public class Config {
+
 }
