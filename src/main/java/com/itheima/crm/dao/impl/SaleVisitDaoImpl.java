4109f61ea20265da3bcfe3b3c15dac48ec70315b
yy598162719 <598162719@qq.com>
2018-07-03 12:23:50 +0800

    1111

--- src/main/java/com/itheima/crm/dao/impl/SaleVisitDaoImpl.java
+++ src/main/java/com/itheima/crm/dao/impl/SaleVisitDaoImpl.java
@@ -1,6 +1,8 @@
 package com.itheima.crm.dao.impl;
 
 import com.itheima.crm.dao.SaleVisitDao;
+import com.itheima.crm.domain.Customer;
+import com.itheima.crm.domain.SaleVisit;
 
-public class SaleVisitDaoImpl implements SaleVisitDao {
+public class SaleVisitDaoImpl  extends BaseDaoImpl<SaleVisit> implements SaleVisitDao {
 }
--- src/main/java/com/itheima/crm/domain/Customer.java
+++ src/main/java/com/itheima/crm/domain/Customer.java
@@ -1,9 +1,12 @@
 package com.itheima.crm.domain;
 
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
 import java.io.InputStream;
 import java.util.HashSet;
 import java.util.Set;
-
+@Data
 public class Customer {
 
 	private Long cust_id;
@@ -23,69 +26,8 @@ public class Customer {
 	
 	// 一的的一端
 	Set<LinkMan> linkMans = new HashSet<>();
-	
-	public Set<LinkMan> getLinkMans() {
-		return linkMans;
-	}
-	public void setLinkMans(Set<LinkMan> linkMans) {
-		this.linkMans = linkMans;
-	}
-	public String getCust_image() {
-		return cust_image;
-	}
-	public void setCust_image(String cust_image) {
-		this.cust_image = cust_image;
-	}
-	public Long getCust_id() {
-		return cust_id;
-	}
-	public void setCust_id(Long cust_id) {
-		this.cust_id = cust_id;
-	}
-	public String getCust_name() {
-		return cust_name;
-	}
-	public void setCust_name(String cust_name) {
-		this.cust_name = cust_name;
-	}
-	public BaseDict getBaseDictSource() {
-		return baseDictSource;
-	}
-	public void setBaseDictSource(BaseDict baseDictSource) {
-		this.baseDictSource = baseDictSource;
-	}
-	public BaseDict getBaseDictIndustry() {
-		return baseDictIndustry;
-	}
-	public void setBaseDictIndustry(BaseDict baseDictIndustry) {
-		this.baseDictIndustry = baseDictIndustry;
-	}
-	public BaseDict getBaseDictLevel() {
-		return baseDictLevel;
-	}
-	public void setBaseDictLevel(BaseDict baseDictLevel) {
-		this.baseDictLevel = baseDictLevel;
-	}
-	public String getCust_phone() {
-		return cust_phone;
-	}
-	public void setCust_phone(String cust_phone) {
-		this.cust_phone = cust_phone;
-	}
-	public String getCust_mobile() {
-		return cust_mobile;
-	}
-	public void setCust_mobile(String cust_mobile) {
-		this.cust_mobile = cust_mobile;
-	}
 
 	private InputStream inputStream;
 
-	public InputStream getInputStream() {
-		return inputStream;
-	}
 
-	public void setInputStream(InputStream inputStream) {
-		this.inputStream = inputStream;
-	}
 }
